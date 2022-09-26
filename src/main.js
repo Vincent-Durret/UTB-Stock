1687b09c4c9a91e005fe288a2334084fa197a88b
GarFunkle <vincent.durret@gmail.com>
2022-09-26 13:02:59 +0200

    change

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from "./App.vue";
 
 import router from "./router";
 
-import { initializeApp } from "firebase/app";
 
 import store from "./store";
 
@@ -11,15 +10,6 @@ import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCyRyUyuuVCs6eEs-U1l3h09R11LQtU-VM",
-  authDomain: "utb-stock.firebaseapp.com",
-  projectId: "utb-stock",
-  storageBucket: "utb-stock.appspot.com",
-  messagingSenderId: "1017476624667",
-  appId: "1:1017476624667:web:3ff490257dcb33505c23be",
-  measurementId: "G-MZCJEL5WNJ",
-};
 
 const options = {
   transition: "Vue-Toastification__fade",
@@ -30,6 +20,6 @@ const options = {
   pauseOnHover: false,
 };
 
-initializeApp(firebaseConfig);
+
 
 createApp(App).use(router).use(store).use(Toast, options).mount("#app");
