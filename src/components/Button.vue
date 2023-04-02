a7f298809dacf2cc80da921f2a662eb0007bd499
GareFunkle <vincent.durret@gmail.com>
2023-04-02 09:18:17 +0200

    add button

---
+++ src/components/Button.vue
@@ -0,0 +1,65 @@
+<template>
+    <button class="btn" :class="type" @click="handleClick">
+        {{ label }}
+    </button>
+</template>
+  
+
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    props: {
+        label: {
+            type: String,
+            required: true,
+        },
+        type: {
+            type: String,
+            default: 'default',
+        },
+    },
+    setup(props, { emit }) {
+        function handleClick() {
+            emit('click');
+        }
+
+        return {
+            handleClick,
+        };
+    },
+});
+</script>
+
+  
+<style scoped>
+.btn {
+    position: relative;
+    z-index: 10000;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    color: #fff;
+    font-weight: bold;
+    cursor: pointer;
+    transition: all 0.3s ease;
+}
+
+.btn.default {
+    background-color: #3273dc;
+}
+
+.btn.default:hover {
+    background-color: #2768c7;
+}
+
+.btn.warning {
+    background-color: #f66;
+}
+
+.btn.warning:hover {
+    background-color: #ef5c00;
+}
+</style>
+  
